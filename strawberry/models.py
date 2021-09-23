a84d998c2df210e0e3ef56e9a3cba8599c1f2ed9
dokeefemain <dokeefe3@student.gsu.edu>
2021-09-23 14:26:14 -0400

    Adding autogenerated userid when creating account to DB

--- strawberry/models.py
+++ strawberry/models.py
@@ -1,16 +1,33 @@
 from django.db import models
 COMMUNITY_CHOICES = [("General","General"),("Baseball","Baseball"),("Basketball","Basketball"),("Tennis","Tennis")]
 class Users(models.Model):
-    username = models.CharField(max_length=12)
-    password = models.CharField(max_length=12)
+    UserName = models.CharField(max_length=12)
+    Password = models.CharField(max_length=12)
+    UserID = models.IntegerField()
+    ImgUrl = models.CharField(max_length=50)
     def __str__(self):
-        return self.username
+        return self.UserID
 
 class Posts(models.Model):
-    #username = models.CharField(max_length=12)
-    #community_id = models.CharField(max_length=12)
-    #post_id = models.IntegerField()
-    title = models.CharField(max_length=255)
-    post = models.TextField()
+    Title = models.CharField(max_length=50)
+    Post = models.TextField()
+    UserID = models.IntegerField()
+    PostID = models.IntegerField()
+    CommunityID = models.IntegerField()
+    ImgUrl = models.CharField(max_length=50)
     def __str__(self):
-        return self.post_id
+        return self.PostID
+
+class Comments(models.Model):
+    Comment = models.TextField()
+    UserID = models.IntegerField()
+    PostID = models.IntegerField()
+    CommentID = models.IntegerField()
+    def __str__(self):
+        return self.CommentID
+
+class Communities(models.Model):
+    CommunityID = models.CharField(max_length=30)
+    CommunityID = models.IntegerField()
+    def __str__(self):
+        return self.CommunityID
--- strawberry/views.py
+++ strawberry/views.py
@@ -4,8 +4,10 @@
 # back to view
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView
-from .models import Users, Posts
+from .models import Users, Posts, Comments, Communities
 from django.views.generic.list import ListView
+import pandas as pd
+from django_pandas.io import read_frame
 # Defining a function which
 # will receive request and
 # perform task depending
@@ -15,13 +17,22 @@ from django.views.generic.list import ListView
 class create_account(CreateView):
     #model = Post
     model = Users
-    fields = '__all__'
+    fields = ['UserName','Password']
     template_name = 'C:/Python/strawberry/strawberry/templates/strawberry/create_user.html'
     success_url = ''
+
+    def save(self):
+        obj = super(create_account, self).save(commit=False)
+        query_results = Users.objects.all()
+        df = read_frame(query_results.values())
+        max_id = df['UserID'].max()+1
+        obj.UserID = max_id
+        obj.save()
+        return obj
 
 class create_post(CreateView):
     #model = Post
     model = Posts
-    fields = ['title','post']
-    template_name = 'C:/Python/strawberry/strawberry/templates/strawberry/post_form.html'
+    fields = ['Community','Title','Post']
+    template_name = 'C:/Python/strawberry/strawberry/templates/strawberry/create_post.html'
     success_url = ''
